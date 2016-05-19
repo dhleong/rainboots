8bf218828c479d0b680587a3e699242653fca549
dhleong <falcone88@gmail.com>
2016-05-19 08:46:30 -0400

    Bump version to 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rainboots "0.4.0"
+(defproject rainboots "0.5.0"
   :description "Protect yourself from the grossness of writing a MUD server"
   :url "http://github.com/dhleong/rainboots"
   :license {:name "Eclipse Public License"
