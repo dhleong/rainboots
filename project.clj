9019b693146c5dcca3c3b720ef56c80bef1f5071
Daniel Leong <me@dhleong.net>
2019-03-03 10:31:44 -0500

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://github.com/dhleong/rainboots"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.3.442"]
-                 [aleph "0.4.3"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.async "0.4.490"]
+                 [aleph "0.4.6"]
                  [gloss "0.2.6"]
-                 [potemkin "0.4.3"]])
+                 [potemkin "0.4.5"]])
